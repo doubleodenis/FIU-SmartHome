0a6e085f13b094675ca5e576414b4eb538a68a71
Denis Ortega <ortega.dc98@gmail.com>
2020-03-08 18:23:06 -0400

    Established basic express server and DB connection

---
+++ connection.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME || "hassio-senior.ch89zlanp2xd.us-east-2.rds.amazonaws.com",
+    user     : process.env.RDS_USERNAME || "admin",
+    password : process.env.RDS_PASSWORD || "0Lp18&Pl&pG#2u3*",
+    // port     : process.env.RDS_PORT || 5000
+    database : 'Smart_Home'
+  });
+  
+
+
+
+function query(q) {
+    connection.connect(function(err) {
+        if (err) {
+            console.error('Database connection failed: ' + err.stack);
+            return;
+        }
+    
+        console.log('Connected to database.');
+    });
+
+    connection.query(q, function (error, results, fields) {
+        if (error) throw error;
+        console.log('RESULT: ', results);
+    });
+    
+    connection.end();
+}
+module.exports = {
+    query
+};  
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    PORT = process.env.PORT || 3001;
+
+const db = require("./connection");
+const app = express();
+
+process.env.NODE_ENV === "production" ? app.use(express.static("energy-io/build")) : null;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use("/api", apiRoutes);
+
+// app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// })
+
+app.listen(PORT, function() {
+    console.log(`Server running on port ${PORT}!`);
+});
+
+db.query('SELECT 1 + 1');
