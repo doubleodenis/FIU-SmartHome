c9a167be43da34ae25a1b7b4e332bc33fa1cfede
Denis Ortega <ortega.dc98@gmail.com>
2020-03-21 18:12:56 -0400

    Testing listening on pcap session

--- scripts/network.js
+++ scripts/network.js
@@ -1,8 +1,9 @@
 const pcap = require('pcap'),
     tcp_tracker = new pcap.TCPTracker(),
     pcap_session = pcap.createSession('wlan0', { filter: "ip proto \\tcp" });
- 
+
 tcp_tracker.on('session', function (session) {
+    console.log(session);
   console.log("Start of session between " + session.src_name + " and " + session.dst_name);
   session.on('end', function (session) {
       console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
@@ -11,8 +12,16 @@ tcp_tracker.on('session', function (session) {
  
 pcap_session.on('packet', function (raw_packet) {
     var packet = pcap.decode.packet(raw_packet);
-    console.log(packet);
+    // console.log(packet);
     tcp_tracker.track_packet(packet);
 });
+
+setTimeout(function () {
+    console.log("Listening on " + pcap_session.device_name);
+}, 5000);
+
+setInterval(function() {
+    console.log(pcap_session.stats());
+}, 10000)
 
 module.exports = pcap_session;
