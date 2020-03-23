5c05c11c64ae87cab6b01023a6d2c01228bcea58
Samuel Yorizzo <Syori001@fiu.edu>
2020-03-23 16:56:08 -0400

    Network script now collects all traffic during tcp session, and pushes it to the database. Throws a non-mysql error

--- scripts/network.js
+++ scripts/network.js
@@ -1,74 +1,85 @@
-const db = require('../connection');
-const pcap = require('pcap');
+var pcap = require("pcap"),
+  pcap_session,
+  tcp_tracker = new pcap.TCPTracker();
+const db = require("../connection");
 const moment = require("moment");
-var pcap_session = null, tcp_tracker = null, devices = [];
+devices = [];
 
 function startNetworkTracking() {
-    //Add ips to device list
-    function addDevice(device) {
-        const d = device.split(':')[0]; //Remove port from ip
-        if(!devices.includes(d)) devices.push(d);
+  //Add ips to device list
+  function addDevice(device) {
+    const d = device.split(":")[0]; //Remove port from ip
+    if (!devices.includes(d)) devices.push(d);
+  }
+  pcap_session = pcap.createSession("wlan0", { filter: "ip proto \\tcp" });
+
+  // listen for packets, decode them, and feed TCP to the tracker
+  pcap_session.on("packet", function(raw_packet) {
+    var packet = pcap.decode.packet(raw_packet);
+    tcp_tracker.track_packet(packet);
+  });
+
+  pcap_session.on("complete", function() {
+    console.log("pcap session complete.");
+  });
+
+  // tracker emits sessions, and sessions emit data
+  tcp_tracker.on("session", function(session) {
+    console.log(
+      "Start of TCP session between " +
+        session.src_name +
+        " and " +
+        session.dst_name
+    );
+
+    let localDeviceIsSrc = null;
+
+    //Adding device to network list if part of AP domain
+    if (session.src.substring(0, 8) === "10.3.141") {
+      addDevice(session.src);
+      localDeviceIsSrc = true;
+    } else if (session.dst.substring(0, 8) === "10.3.141") {
+      addDevice(session.dst);
+      localDeviceIsSrc = false;
     }
 
-    tcp_tracker = new pcap.TCPTracker();
-    pcap_session = pcap.createSession('wlan0', { filter: "ip proto \\tcp" });
-    
-    tcp_tracker.on('session', function (session) {
-
-        let localDeviceIsSrc = null;
-
-        //Adding device to network list if part of AP domain
-        if(session.src.substring(0,8) === "10.3.141") {
-            addDevice(session.src);
-            localDeviceIsSrc = true;
-        }
-        else if(session.dst.substring(0,8) === "10.3.141") {
-            addDevice(session.dst);
-            localDeviceIsSrc = false;
-        }
-
-        //Adding into network table
-        //If src
-        if(localDeviceIsSrc == true) {
-            //id_network, user_id, received_bytes, sent bytes, ip_address, time
-            const sent_bytes = session.sent_bytes_tcp;
-            const received_bytes = session.recv_bytes_tcp;
-            const ip_address = session.src.split(":")[0]; //Removing port from ip
-            const time = "'" + moment(new Date()).format("YYYY-MM-DD HH:mm:ss") + "'";
-            db.insert(`INSERT INTO Network (ip_address, sent_bytes, received_bytes, time) VALUES (${ip_address}, ${sent_bytes}, ${received_bytes}, ${time})`);
-        }
-        //device is destination so sent and received are flipped?
-        else if(localDeviceIsSrc == false) {
-            const sent_bytes = session.recv_bytes_tcp; 
-            const received_bytes = session.sent_bytes_tcp;
-            const ip_address = session.src.split(":")[0]; //Removing port from ip
-            const time = "'" + moment(new Date()).format("YYYY-MM-DD HH:mm:ss") + "'";
-            db.insert(`INSERT INTO Network (ip_address, sent_bytes, received_bytes, time) VALUES (${ip_address}, ${sent_bytes}, ${received_bytes}, ${time})`);
-        }
-        //Neither src or dest is coming from AP?
-        else {
-            console.log(`DEBUG: Unidentified ip on network? SRC: ${session.src} DEST: ${session.dst}`);
-        }
-        
-    
-    //   console.log("Start of session between " + session.src_name + " and " + session.dst_name);
-      session.on('end', function (session) {
-          console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
-      });
+    session.on("end", function(session) {
+      const sent_bytes = session.send_bytes_payload;
+      const received_bytes = session.recv_bytes_payload;
+      const ip_address = session.src.split(":")[0]; //Removing port from ip
+      const time = "'" + moment(new Date()).format("YYYY-MM-DD HH:mm:ss") + "'";
+      let val =
+        "'" +
+        ip_address +
+        "'" +
+        ", " +
+        sent_bytes +
+        ", " +
+        received_bytes +
+        "," +
+        time;
+      console.log(
+        `INSERT INTO Network (ip_address, sent_bytes, received_bytes, time) VALUES (${ip_address}, ${sent_bytes}, ${received_bytes}, ${time})`
+      );
+      db.insert(
+        `INSERT INTO Network (ip_address, sent_bytes, received_bytes, time) VALUES (${val})`
+      );
+      console.log(
+        "End of TCP session between " +
+          session.src_name +
+          " and " +
+          session.dst_name
+      );
     });
-     
-    pcap_session.on('packet', function (raw_packet) {
-        var packet = pcap.decode.packet(raw_packet);
-        tcp_tracker.track_packet(packet);
-    });
+  });
 
-    return {
-        pcap_session,
-        tcp_tracker,
-        devices
-    }
+  return {
+    pcap_session,
+    tcp_tracker,
+    devices
+  };
 }
 
 module.exports = {
-    startNetworkTracking
+  startNetworkTracking
 };
