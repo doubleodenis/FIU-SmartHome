e5262ee14f32e5cc5c9deaaeeede62e121eaa6fb
Samuel Yorizzo <Syori001@fiu.edu>
2020-04-07 00:00:15 -0400

    Now roundup time in 5 second intervals on inserts, and wrote stored procedure that adds energy usage and network traffic

--- scripts/EnergyUsageWemo.js
+++ scripts/EnergyUsageWemo.js
@@ -7,26 +7,33 @@ function foundDevice(err, device) {
   if (device.deviceType === Wemo.DEVICE_TYPE.Insight) {
     //console.log("Wemo Insight Switch found: %s", device.friendlyName);
     var client = this.client(device);
-    client.on("insightParams", function(state, power) {
-     // console.log(
-     //   "%s’s power consumption: %s W",
-     //   this.device.friendlyName,
-     //   power
-     // );
+    client.on("insightParams", function (state, power) {
+      // console.log(
+      //   "%s’s power consumption: %s W",
+      //   this.device.friendlyName,
+      //   power
+      // );
 
-      let date = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+      let date = moment();
+
+      const remainder = 5 - (date.seconds() % 5);
+      date = moment(date)
+        .add(remainder, "seconds")
+        .format("YYYY-MM-DD HH:mm:ss");
+
       let val =
-        power +
-        ", " +
         "'" +
-        date +
-        "', '" +
         device.friendlyName +
         "', '" +
         device.UDN.substring(17) +
-        "'";
-      var sql = `INSERT INTO Energy (energy, date, device_name, device_serial_number) VALUES (${val})`;
-
+        "'," +
+        power +
+        ",'" +
+        date +
+        "', " +
+        "1";
+      var sql = `Call 5Energy(${val})`;
+      console.log(sql);
       db.insert(sql);
     });
   }
--- scripts/network.js
+++ scripts/network.js
@@ -9,17 +9,17 @@ function startNetworkTracking() {
   pcap_session = pcap.createSession("wlan0", { filter: "ip proto \\tcp" });
 
   // listen for packets, decode them, and feed TCP to the tracker
-  pcap_session.on("packet", function(raw_packet) {
+  pcap_session.on("packet", function (raw_packet) {
     var packet = pcap.decode.packet(raw_packet);
     tcp_tracker.track_packet(packet);
   });
 
-  pcap_session.on("complete", function() {
+  pcap_session.on("complete", function () {
     console.log("pcap session complete.");
   });
 
   // tracker emits sessions, and sessions emit data
-  tcp_tracker.on("session", function(session) {
+  tcp_tracker.on("session", function (session) {
     console.log(
       "Start of TCP session between " +
         session.src_name +
@@ -27,11 +27,18 @@ function startNetworkTracking() {
         session.dst_name
     );
 
-    session.on("end", function(session) {
+    session.on("end", function (session) {
       const sent_bytes = session.send_bytes_payload;
       const received_bytes = session.recv_bytes_payload;
       const ip_address = session.src.split(":")[0]; //Removing port from ip
-      const time = "'" + moment(new Date()).format("YYYY-MM-DD HH:mm:ss") + "'";
+
+      let date = moment();
+
+      const remainder = 5 - (date.seconds() % 5);
+      date = moment(date)
+        .add(remainder, "seconds")
+        .format("YYYY-MM-DD HH:mm:ss");
+
       let val =
         "'" +
         ip_address +
@@ -40,14 +47,14 @@ function startNetworkTracking() {
         sent_bytes +
         ", " +
         received_bytes +
-        "," +
-        time;
-      console.log(
-        `INSERT INTO Network (ip_address, sent_bytes, received_bytes, time) VALUES (${ip_address}, ${sent_bytes}, ${received_bytes}, ${time})`
-      );
-      db.insert(
-        `INSERT INTO Network (ip_address, sent_bytes, received_bytes, time) VALUES (${val})`
-      );
+        ",'" +
+        date +
+        "', 1";
+
+      let sql = `Call 5Network(${val})`;
+      console.log(sql);
+      db.insert(sql);
+
       console.log(
         "End of TCP session between " +
           session.src_name +
@@ -60,10 +67,10 @@ function startNetworkTracking() {
   return {
     pcap_session,
     tcp_tracker,
-    devices
+    devices,
   };
 }
 
 module.exports = {
-  startNetworkTracking
+  startNetworkTracking,
 };
